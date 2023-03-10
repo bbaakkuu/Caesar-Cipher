ce2c9b23f9cea53e50abe40333d64e1e76ee90db
bbaakkuu <shubhayanray20131014@gmail,com>
2023-03-10 18:09:22 +0530

    change

--- caesarcipher.java
+++ caesarcipher.java
@@ -1,5 +1,10 @@
 import java.util.Scanner;
 public class caesarcipher
 {
-    Scanner in = new Scanner(System.in);
+    public static void main(String[] args) 
+    {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter message:");
+        
+    }
 }
