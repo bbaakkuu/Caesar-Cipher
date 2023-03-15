b33821d6e421ae491d4f517d94ae2f40f5ac31b4
bbaakkuu <shubhayanray20131014@gmail,com>
2023-03-15 22:28:46 +0530

    test

--- caesarcipher.java
+++ caesarcipher.java
@@ -1,36 +1,51 @@
 //I have looked over and its certainly possible to make a caesar cipher with shifts chosen by the user.
-// MAXIMUM SHIFTS CAN BE 25
+// MAXIMUM SHIFTS CAN BE 25.
 import java.util.Scanner;
 public class caesarcipher
 {
-    private static String library(String x, int key){
-        String cipher = "";
-        x = x.toUpperCase();
-        char e = 'A';
-        for (int i=0; i<x.length(); i++) {
-            e = x.charAt(i);
-            if (Character.isLetter(e))
-                e = (char) ('A' + (e - 'A' + key) % 26);
-            cipher += e;
-        }
-        return cipher;
+    private char [] alpha = {'A','B','C','D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', ' '};
+    private int []encrpt = new int[1000];// what is the purpose of the value '1000'
+    void initializer(int index, int val)
+    {
+        encrpt[index] = val;
     }
-    public static void main(String[] args)
+
+    static char library(char x, int len, int key)
     {
         caesarcipher cc = new caesarcipher();
+        int in=0;
+        for(int j=0; j<len; j++)
+        {
+            for(int i=0; i<=26; i++)
+            {
+                if(cc.alpha[i] == x)
+                {
+                    in = i+key;
+                }
+                cc.initializer(j, in);
+            }
+        }
+        return 'a';
+    }
+    public static void main(String[] args) 
+    {
         Scanner in = new Scanner(System.in);
         System.out.print("Enter message: ");
         String n = in.nextLine();
         System.out.print("Enter no. of shifts: ");
         int key = in.nextInt();
-        String cipher = cc.library(n, key);
-        System.out.println(cipher);
-        in.close();
+        int l = n.length();
+        int i;char sep, store;
+        for(i = 0; i<l; i++)
+        {
+            sep = n.charAt(i);
+            store = library(sep, l, key);
+        }
     }
 }
 
 /*
  * for(int ele: arr){
- *
+ * 
  * }
  */
