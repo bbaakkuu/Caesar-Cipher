191097204058b57658fd517a81f704cac9617d29
bbaakkuu <shubhayanray20131014@gmail,com>
2023-03-15 08:53:14 +0530

    q1

--- caesarcipher.java
+++ caesarcipher.java
@@ -1,19 +1,25 @@
-//I have looked over and its certainly possible to make a caesar cipher with shifts chosen by the user
+//I have looked over and its certainly possible to make a caesar cipher with shifts chosen by the user.
+// MAXIMUM SHIFTS CAN BE 25
 import java.util.Scanner;
 public class caesarcipher
 {
     private char [] alpha = {'A','B','C','D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', ' '};
-    private int []encrpt = new int[1000];
-    void initializer(int index, int val){
+    private int []encrpt = new int[1000];// what is the purpose of the value '1000'
+    void initializer(int index, int val)
+    {
         encrpt[index] = val;
     }
 
-    static char library(char x, int len, int key){
+    static char library(char x, int len, int key)
+    {
         caesarcipher cc = new caesarcipher();
         int in=0;
-        for(int j=0; j<len; j++){
-            for(int i=0; i<=26; i++){
-                if(cc.alpha[i] == x){
+        for(int j=0; j<len; j++)
+        {
+            for(int i=0; i<=26; i++)
+            {
+                if(cc.alpha[i] == x)
+                {
                     in = i+key;
                 }
                 cc.initializer(j, in);
